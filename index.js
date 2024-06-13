69ee07ddf8b4793c9e9de7601c8da971c98d13a0
Debanil Chanda <160310513+Debanx3@users.noreply.github.com>
2024-06-13 15:35:59 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -13,3 +13,17 @@ function closemodal()
 }
 var c=document.querySelector(".trojan");
 c.addEventListener('click',closemodal);
+
+/*STICKY HEADER CODE*/
+const stickpane = document.querySelector('#stickyheader');
+const sticky = stickpane.offsetTop;
+console.log(sticky);
+/*CONSOLE FN GIVES ME OFFSET VALUED DISPLAYED UNDER CONSOLE TAB IN BRAVE V* DEVELOPER TOOLS*/
+window.addEventListener('scroll', ()=>
+    {
+        if(window.pageYoffset > sticky)
+            {stickpane.classList.add('sticky');}
+        else
+            {stickpane.classList.add('sticky');}
+    }
+);
